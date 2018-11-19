fc36ddffa401c982fdc77dd09671190942c5ed15
ozawa0410 <38830349+ozawa0410@users.noreply.github.com>
2018-11-19 12:12:40 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,6 +1,4 @@
-
 <?php
-	
   $json_str = file_get_contents('php://input'); //接收request的body
   $json_obj = json_decode($json_str); //轉成json格式
   
@@ -30,6 +28,4 @@
   curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
   $result = curl_exec($ch);
   curl_close($ch);
-
-
- ?>
+?>
